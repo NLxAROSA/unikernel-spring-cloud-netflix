e71489c596dab8f8f02464052c7eb8b794e14908
NLxAROSA <rosenql@gmail.com>
2016-01-07 19:03:38 +0100

    Initial commit.

---
+++ src/main/java/io/bymbl/service/registration/ServiceRegistrationServiceApp.java
@@ -0,0 +1,19 @@
+package io.bymbl.service.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Author: lars
+ * Since: 17-11-15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistrationServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistrationServiceApp.class, args);
+    }
+}
